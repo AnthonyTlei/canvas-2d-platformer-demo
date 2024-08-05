10f24eb486abaa1a2e1e32466734b8cddc9a0e9e
Anthony Tleiji <anthonytleiji@gmail.com>
2024-08-05 23:09:07 +0300

    Add Player Movement

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ canvas.height = 64 * 9; // 576;
 
 const player = new Player();
 
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  lastKey: "",
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.velocity.x = 0;
+
+  if (keys.d.pressed && keys.lastKey === "d") {
+    player.velocity.x = 2;
+  } else if (keys.a.pressed && keys.lastKey === "a") {
+    player.velocity.x = -2;
+  }
 
   player.draw();
   player.update();
